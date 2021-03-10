2b7855a802e5ae6fb16b125b0dea5e9b9ccbbadf
rebeljedi999 <programmer.wells@gmail.com>
2021-03-10 15:47:39 -0500

    added ml-server components for python

---
+++ TeamC/Intermediate/Build/Win64/TeamCEditor/Development/Engine/SharedPCH.Engine.RTTI.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29910
+#include "C:/Users/rebel/Documents/GitHub/TheCTeam/TeamC/Intermediate/Build/Win64/TeamCEditor/Development/Engine/SharedPCH.Engine.RTTI.ShadowErrors.h"
---
+++ TeamC/Source/TeamC/Blank.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Blank.h"
+
+Blank::Blank()
+{
+}
+
+Blank::~Blank()
+{
+}
